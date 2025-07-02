df7c76af6a9ce5f20258cb78c26f7e2a288bc396
monboga <monroygabriel9@gmail.com>
2025-07-02 15:47:57 -0600

    release: se agregaron colisiones con los ggameObjects y texturas de testing

---
+++ Assets/KinectView/Scripts/Game/Hand.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+
+    public Transform mHandMesh;
+    public Sprite Hands;
+
+    private void Update()
+    {
+        mHandMesh.position = Vector3.Lerp(mHandMesh.position, transform.position, Time.deltaTime * 15.0f);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Bubble"))
+            return;
+        Bubble bubble = collision.gameObject.GetComponent<Bubble>();
+        StartCoroutine(bubble.Pop());
+    }
+
+
+}
